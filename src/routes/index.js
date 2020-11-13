bb6d66e1459b019dde3e12a0b1bf8ef6c6404fd7
unknown <wil.macedo.sa@gmail.com>
2020-11-13 01:28:14 -0300

    Adicionando rotas

---
+++ src/routes/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from '../pages/Home';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initalRouteName={'Home'}
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
